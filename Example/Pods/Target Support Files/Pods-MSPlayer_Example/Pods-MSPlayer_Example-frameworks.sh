53de7ddc63298cf3dee29dedc0b61603ad7bdd26
mason <mason.chang@appimc.com>
2018-05-03 11:55:49 +0800

    fix MSPM Config options and value and delete snapkit

--- Example/Pods/Target Support Files/Pods-MSPlayer_Example/Pods-MSPlayer_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MSPlayer_Example/Pods-MSPlayer_Example-frameworks.sh
@@ -104,12 +104,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MSPlayer/MSPlayer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MSPlayer/MSPlayer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
