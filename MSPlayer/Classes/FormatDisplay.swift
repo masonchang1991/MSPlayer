ccfb802083718cd464bd689f792e0b9f8433fbf9
mason <mason.chang@appimc.com>
2019-05-30 18:26:07 +0800

    [Update] make some change at formatSecondsToString

--- MSPlayer/Classes/FormatDisplay.swift
+++ MSPlayer/Classes/FormatDisplay.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct FormatDisplay {
     static func formatSecondsToString(_ seconds: TimeInterval) -> String {
-        let min = Int(seconds / 60)
-        let sec = Int(seconds.truncatingRemainder(dividingBy: 60))
+        let min = Int(floor(seconds) / 60)
+        let sec = Int(floor(seconds).truncatingRemainder(dividingBy: 60))
         return String(format: "%02d:%02d", min, sec)
     }
 }
